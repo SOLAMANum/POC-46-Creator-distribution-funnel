2839f5895a740d6f2d1d44f71b7cc266c295370b
Solomanum <solomanum@gmail.com>
2026-06-04 13:42:06 +0530

    Update project

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  outputFileTracingRoot: path.resolve(__dirname),
   turbopack: {
     root: path.resolve(__dirname),
   },
